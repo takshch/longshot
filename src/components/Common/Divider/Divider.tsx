8274c4f2ce7cd8bbd9ae45d68284203d5c51af22
takshch <takshch089@gmail.com>
2022-12-03 01:02:41 +0530

    Adds headings

---
+++ src/components/Common/Divider/Divider.tsx
@@ -0,0 +1,5 @@
+import classes from './Divider.module.scss';
+
+const Divider = () => <div className={classes['divider']}></div>;
+
+export default Divider;
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,5 +1,21 @@
+import Divider from '../components/Common/Divider/Divider';
 import HomeLayout from '../layout/HomeLayout';
+import classes from './IndexPage.module.scss';
 
-const IndexPage = () => <HomeLayout>Home Page</HomeLayout>;
+const IndexPage = () => (
+  <HomeLayout>
+    <div className={classes['breadcrumbs']}>
+      <span>Keyword Explorer</span>
+      <span>{'>'}</span>
+      <span className={classes['color-black']}>Keyword Overview</span>
+    </div>
+    <h3 className={classes['keyword-search']}>
+      <span className={classes['color-black']}>Keyword:</span>{' '}
+      <span>shopping in barcelona</span>
+    </h3>
+    <div className={classes['font-short']}>Database: United States</div>
+    <Divider />
+  </HomeLayout>
+);
 
 export default IndexPage;
