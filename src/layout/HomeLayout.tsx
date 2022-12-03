b847c704736af2ead5829d4c437294fa8167ada8
takshch <takshch089@gmail.com>
2022-12-03 21:49:19 +0530

    Fix overflow issue

--- src/layout/HomeLayout.tsx
+++ src/layout/HomeLayout.tsx
@@ -1,9 +1,8 @@
 import Main from '../components/Main/Main';
 import Menu from '../components/Menu/Menu';
-import classes from './HomeLayout.module.scss'
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => (
-  <div className={classes['home-layout']}>
+  <div>
     <Menu />
     <Main>{children}</Main>
   </div>
